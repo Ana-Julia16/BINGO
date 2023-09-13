a0bfe090ae1fc17dc2abdd8bea72c2a25832a9bf
Ana-Julia16 <123672228+Ana-Julia16@users.noreply.github.com>
2023-09-13 18:18:56 -0300

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Rm20222930058
+ * Date: 17/05/2023
+ * Time: 10:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bingo
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
